c59ed249a05ce115e18278e4aaf768a43c19170e
Matt <matt.garvin.416@gmail.com>
2024-01-29 14:43:39 -0500

    first take

--- lib/main.dart
+++ lib/main.dart
@@ -30,14 +30,78 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  
+  // final inputBox = GlobalKey<_InputBoxState>();
+  String label = 'apple';
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          child: Text('ok'),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              InputBox( label, ( value ) { 
+                  label = value; 
+                  setState(() {
+                    
+                  });
+                } 
+              ),
+              Text('list of fruits'),
+              ElevatedButton(
+                onPressed: () {
+                  //InputBox.kkey[1]
+
+                },
+                child: Text( label ),
+              ),
+            ]
+          ),
         ),  
+      ),
+    );
+  }
+}
+
+class InputBox extends StatefulWidget {
+  
+  InputBox( String this.lbl, this.changedValue );
+  String lbl;
+  Function(String value) changedValue;
+
+  @override
+  State<InputBox> createState() => _InputBoxState();
+}
+
+class _InputBoxState extends State<InputBox> {
+
+  final controller1 = TextEditingController();
+  String new_val = '';
+
+  @override
+  void dispose() {
+    controller1.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: controller1,
+        onChanged: (text) { 
+          new_val = controller1.text; 
+          widget.changedValue( new_val );
+          print('>>> ${ controller1.text.toString() }'); 
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          //labelText: 'Password',
+        ),
       ),
     );
   }
